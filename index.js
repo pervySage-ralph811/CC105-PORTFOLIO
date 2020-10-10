b9212c19915a9240a7ac1d08b0da77b882fb07d8
pervySage-ralph811 <72640602+pervySage-ralph811@users.noreply.github.com>
2020-10-10 18:57:37 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h2>Samole JS</h2>
+
+<p id="demo">Hello annoying spiderman.</p>
+
+<button type="button" onclick='document.getElementById("demo").innerHTML = "Hello Batman and superman!"'>Click Me!</button>
+
+</body>
+</html>
